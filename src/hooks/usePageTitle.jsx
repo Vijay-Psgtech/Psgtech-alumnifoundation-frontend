e1b940e05eb09bea58cb267311b5238b161f064f
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-04-28 14:26:41 +0530

    usePageTitle custom hooks has been added for dynamic title.

---
+++ src/hooks/usePageTitle.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react'
+
+const usePageTitle = (title) => {
+  useEffect(() => {
+    document.title = `${title} | PSG TECH ALUMNI FOUNDATION`;
+  }, [title]);
+};
+
+export default usePageTitle
