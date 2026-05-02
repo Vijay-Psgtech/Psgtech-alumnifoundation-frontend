73ee4a6a09cc23e1d34a4b1fd073c57f62d8fc85
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-05-02 09:26:46 +0530

    Patrons section images file path changed & other images also added.

--- src/content/data/PatronsData.js
+++ src/content/data/PatronsData.js
@@ -1,8 +1,14 @@
-import patronImg1 from "../../assets/Images/2400ce49a3e248e589411861b3edbc9a.png";
-import patronImg2 from "../../assets/Images/403896fc0bc94379b9423d9548647246.png";
-import patronImg3 from "../../assets/Images/e03cfeb9399c48e68275cd0a42e02ea1.jpg";
-import patronImg4 from "../../assets/Images/7b9ac6007dc34b258021c038b4cba463.png";
-import patronImg5 from "../../assets/Images/639db881ab7e484c97ec021f9944983a.png";
+import patronImg1 from "../../assets/Images/patrons/founder.png";
+import patronImg2 from "../../assets/Images/patrons/chief.png";
+import patronImg3 from "../../assets/Images/patrons/patron.jpg";
+import patronImg4 from "../../assets/Images/patrons/associate.jpeg";
+import patronImg5 from "../../assets/Images/patrons/honorary1.png";
+import patronImg6 from "../../assets/Images/patrons/honorary2.png";
+import patronImg7 from "../../assets/Images/patrons/honorary3.png";
+import patronImg8 from "../../assets/Images/patrons/honorary4.png";
+import patronImg9 from "../../assets/Images/patrons/honorary5.png";
+import patronImg10 from "../../assets/Images/patrons/honorary6.png";
+import patronImg11 from "../../assets/Images/patrons/honorary7.png";
 
 export const Patrons = [
   {
@@ -27,17 +33,59 @@ export const Patrons = [
     image: patronImg3,
   },
   {
-    name: "Dr. K. Venkataraman",
-    role: "Honorary Patron",
-    note: "",
-    bio: "A long-standing supporter of PSG's academic initiatives and outreach.",
+    name: "Dr B Giriraj",
+    role: "Associate Patron",
+    note: "BE '92",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
     image: patronImg4,
   },
   {
-    name: "Dr. A. Shanmugasundaram",
+    name: "Dr R Subbayyan",
     role: "Honorary Patron",
-    note: "BE '57",
-    bio: "Champion for alumni-student engagement and institutional service.",
+    note: "Ph.D. '68",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
     image: patronImg5,
+  },
+  {
+    name: "Dr K Venkataraman",
+    role: "Honorary Patron",
+    note: "",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
+    image: patronImg6,
+  },
+  {
+    name: "Dr A Shanmugasundaram",
+    role: "Honorary Patron",
+    note: "BE'57",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
+    image: patronImg7,
+  },
+  {
+    name: "Dr  S Subramanyan",
+    role: "Honorary Patron",
+    note: "BE'59",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
+    image: patronImg8,
+  },
+  {
+    name: "Dr P Radhakrishnan",
+    role: "Honorary Patron",
+    note: "Ph.D. '76",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
+    image: patronImg9,
+  },
+  {
+    name: "Dr S Vijayarangan",
+    role: "Honorary Patron",
+    note: "ME'78",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
+    image: patronImg10,
+  },
+  {
+    name: "Dr R Rudramoorthy",
+    role: "Honorary Patron",
+    note: "Ph D'95",
+    bio: "A patron of research and student mentorship across decades of academic progress.",
+    image: patronImg11,
   },
 ];
